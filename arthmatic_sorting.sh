1b58f703f6ecfdeb65781e3d67f633387c86894c
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-17 22:46:26 +0530

    useCase1

--- arthmatic_sorting.sh
+++ arthmatic_sorting.sh
@@ -1,6 +1,11 @@
 # Arthmatic Computation & sorting
-read -p "Enter first number" a
-read -p "Enter 2nd number" b
-read -p "Enter 3rd number" c
+read -p "Enter first number: " a
+read -p "Enter 2nd number: " b
+read -p "Enter 3rd number: " c
 echo "User inputs  $a $b $c"
+
+# use_case1
+#first 1 opreration
+R1=$(($a+$b*$c)); echo $R1
  
+
