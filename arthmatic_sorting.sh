636eb254ff10e2ab0b85d12e4206564a1c861d09
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-18 01:47:09 +0530

    useCase3

--- arthmatic_sorting.sh
+++ arthmatic_sorting.sh
@@ -5,12 +5,13 @@ read -p "Enter 3rd number: " c
 echo "User inputs  $a $b $c"
 
 # use_case1
-#first 1 opreration
+
 R1=$(($a+$b*$c)); echo $R1
 
 
 #use_case_2
-#second operation
+
 R2=$(($c+$a/$b)); echo $R2
  
-
+#usecase_3
+R3=$(($a%$b+$c)); echo $R3
